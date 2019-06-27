fa9305dc2aadd79afd659edbd82d30d4dd2d198c
Tsuboi Shusuke <tsuboi.s.ab@is-mac34.edu.c.titech.ac.jp>
2019-06-27 13:04:48 +0900

    quick_sort.c

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -18,6 +18,20 @@ void swap(int *p, int *q){
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+    if (n == 0) {}
+    else{
+        int i,j,pivot;
+        pivot = A[0];
+        for(i = j =1; i < n ; i++){
+            if(A[i] <= pivot){
+                swap(A+i,A+j);
+                j++;
+                }
+            }
+            swap(A , A+j-1);
+            quick_sort(A  ,j - 1);
+            quick_sort(A+j , n - j);
+    }
 }
 
 int main(){
@@ -26,10 +40,20 @@ int main(){
   A[1] = 17; //原始元
   for(i=2;i<N;i++){
     A[i] = (long long int) A[i-1] * A[1] % N;
+    //printf("%d\n",A[i]);
   }
 
   quick_sort(A, N);
   for(i=0;i<N;i++){
+    //printf("%d\n",A[i]);
     if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
   }
 }
+
+
+
+
+
+
+
+

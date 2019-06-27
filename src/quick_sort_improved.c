9be02c9fd5db127e99404dc507be204920fa6860
Tsuboi <tsuboi.s.ab@is-mac34.edu.c.titech.ac.jp>
2019-06-27 18:56:38 +0900

    quick_sort_improved.c

--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -18,6 +18,45 @@ void swap(int *p, int *q){
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+    if (n <= 0) {}
+    else{
+        int i,j,l,pivot;
+        
+ /*    
+        pivot = A[0];
+        for(i = j =1; i < n ; i++){
+            if(A[i] <= pivot){
+                swap(A+i,A+j);
+                j++;
+                }
+            }
+            swap(A , A+j-1);
+            quick_sort(A  ,j - 1);
+            quick_sort(A+j , n - j);
+
+
+
+
+*/
+        pivot = A[0];
+        for(j = 0, i = l = 1; i < n ; i++){
+            if(A[i] < pivot){
+                swap(A+i,A+l);
+                swap(A+l,A+j);
+                j++;
+                l++;
+            }
+            else if (A[i] == pivot){
+                swap(A+l,A+i);
+                l++;
+            }
+        }
+        
+
+        quick_sort(A  ,j );
+        quick_sort(A+l , n - l);
+
+    }
 }
 
 int main(){
@@ -26,12 +65,15 @@ int main(){
   A[1] = 17; //原始元
   for(i=2;i<N;i++){
     A[i] = (long long int) A[i-1] * A[1] % N;
+    //printf("%d\n",A[i]);
   }
+  quick_sort(A, N);
+  //printf("\n");
 
 // すべての要素が同じ場合でも計算が早く終わるか確認する
 
-  quick_sort(A, N);
   for(i=0;i<N;i++){
+    //printf("%d\n",A[i]);
     if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
   }
 }
